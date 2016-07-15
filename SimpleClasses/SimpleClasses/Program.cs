a459d43504f3c0fe42a21086f5a75ea01e60ade0
Will Morris <morriswr01@gmail.com>
2016-07-15 21:53:20 +0100

    More C# Practice

--- SimpleClasses/SimpleClasses/Program.cs
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -20,9 +20,21 @@ namespace SimpleClasses
             myCar.Year = 1986;
             myCar.Color = "Silver";
 
-            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            //decimal value = DetermineMarketValue(myCar);
+            //Console.WriteLine("{0:C}", value);
+
+            //Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue()); 
+
             Console.ReadLine();
 
+        }
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+            //Someday i might look up the car using an online webservice to get accurate value.
         }
     }
     class Car
@@ -31,6 +43,21 @@ namespace SimpleClasses
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+                carValue = 1000;
+            else
+                carValue = 2000;
+
+            return carValue;
+                {
+
+            }
+        }
+
 
     }
 }
