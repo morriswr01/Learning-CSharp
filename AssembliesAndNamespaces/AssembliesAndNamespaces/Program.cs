067053cf9b4f923073c6d85a5db8cb12056af0ef
Will Morris <morriswr01@gmail.com>
2016-08-01 16:01:37 +0100

    more c# practice

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,26 @@
+﻿//These are namespaces 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string address = "https://www.facebook.com";
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+            File.WriteAllText(@"E:\Documents\GitHub\someText.txt", reply);
+            Console.WriteLine(reply);
+            Console.ReadLine();
+        }
+    }
+}
