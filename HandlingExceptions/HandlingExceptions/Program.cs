4d0876407d6ffbe28a2b73374e147de8b1e75c13
Will Morris <morriswr01@gmail.com>
2016-08-31 21:42:05 +0100

    more c# practise

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string content = File.ReadAllText(@"E:\Documents\GitHub\someTex.txt");
+                Console.WriteLine(content);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error wront thing");
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
